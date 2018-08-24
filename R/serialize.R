d5e7f8105fd3429567762d5fc3b320e7c99bf2f9
dipterix <dipterix.wang@gmail.com>
2018-08-24 11:27:13 -0500

    functions to convert R object to data URI

--- R/serialize.R
+++ R/serialize.R
@@ -1,7 +1,33 @@
+
+#' File or any R objects to Data URI, base64 encoded
 #' @export
-as_data_uri <- function(obj, mime = "text/plain;charset=US-ASCII", ...){
-  sprintf('data:%s;base64,%s', mime, jsonlite::base64_enc(jsonlite::toJSON(obj, ...)))
+as_data_uri <- function(obj, mime = "text/plain;charset=US-ASCII", is_file = FALSE, ...){
+  if(is_file){
+    con = file(obj, "rb")
+    on.exit(close(con))
+    if (isTRUE(summary(con)$text == "binary")) {
+      l <- raw()
+      while (length(r <- readBin(con, raw(0), 1048576L))) l <- c(l, r)
+      data = l
+    }else{
+      data = readLines(con)
+      data = paste(data, collapse = '\n')
+      data = jsonlite::toJSON(data)
+    }
+  }else{
+    data = jsonlite::toJSON(obj, ...)
+  }
+  sprintf(
+    'data:%s;base64,%s',
+    mime,
+    jsonlite::base64_enc(data)
+  )
 }
+
+
+# as_data_uri <- function(obj, mime = "text/plain;charset=US-ASCII", ...){
+#   sprintf('data:%s;base64,%s', mime, jsonlite::base64_enc(jsonlite::toJSON(obj, ...)))
+# }
 
 #' @export
 as_data_uri_file <- function(file, ...){
